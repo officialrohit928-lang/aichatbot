4f96609501f26d5eb345f9e7785a0e1aa07eb791
officialrohit928-lang <officialrohit928@gmail.com>
2026-02-11 10:27:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,10 +24,10 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     }
 
     payload = {
-        "model": "llama3-8b-8192",
-        "messages": [
-            {"role": "user", "content": user_text}
-        ]
+    "model": "llama-3.1-8b-instant",
+    "messages": [
+        {"role": "user", "content": user_text}
+    ]
     }
 
     r = requests.post(URL, headers=headers, json=payload, timeout=20)
